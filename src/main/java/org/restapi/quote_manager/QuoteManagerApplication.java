bb991734cce782a4eef5393e6b2327be746d6d40
Harshith N J <njharshith122@gmail.com>
2025-02-13 17:53:30 +0530

    Project Created

---
+++ src/main/java/org/restapi/quote_manager/QuoteManagerApplication.java
@@ -0,0 +1,13 @@
+package org.restapi.quote_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuoteManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuoteManagerApplication.class, args);
+	}
+
+}
