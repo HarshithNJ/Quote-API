a5dd38be3b01fafe1a74a5ac6a28f14105058cd0
Harshith N J <njharshith122@gmail.com>
2025-02-14 00:27:47 +0530

    Add a quote

--- src/main/java/org/restapi/quote_manager/controller/quoteController.java
+++ src/main/java/org/restapi/quote_manager/controller/quoteController.java
@@ -1,7 +1,11 @@
 package org.restapi.quote_manager.controller;
 
+import org.restapi.quote_manager.dto.quote;
 import org.restapi.quote_manager.service.quoteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,4 +13,11 @@ public class quoteController {
     
     @Autowired
     quoteService service;
+
+
+    // to Add a new quote
+    @PostMapping("/quotes")
+    public ResponseEntity<Object> addQuote(@RequestBody quote quote){
+        return service.addQuote(quote);
+    }
 }
--- src/main/java/org/restapi/quote_manager/service/quoteService.java
+++ src/main/java/org/restapi/quote_manager/service/quoteService.java
@@ -1,7 +1,13 @@
 package org.restapi.quote_manager.service;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.restapi.quote_manager.dto.quote;
 import org.restapi.quote_manager.repository.quoteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,4 +15,21 @@ public class quoteService {
     
     @Autowired
     quoteRepository repository;
+
+    public ResponseEntity<Object> addQuote(quote quote) {
+        if(repository.existsByText(quote.getText())){
+            Map<String, Object> map = new HashMap<String, Object>();
+            map.put("error", "Quote already exists");
+
+            return new ResponseEntity<Object>(map, HttpStatus.NOT_ACCEPTABLE);
+        }else{
+            repository.save(quote);
+
+            Map<String, Object> map = new HashMap<String, Object>();
+            map.put("success", "Quote added successfully");
+            map.put("Quote", quote);
+
+            return new ResponseEntity<Object>(map, HttpStatus.CREATED);
+        }
+    }
 }
